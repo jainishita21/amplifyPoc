7c05fdca5cd7d66e13cbfa5b4d0e47181405ce6d
jainishita21 <jnishita21@gmail.com>
2022-08-20 16:31:01 +0530

    add route

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import Home from './Home';
-import About from './About';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Nav from './Components/Nav';
 
